3ffba445021583b575f138798462e56bf3479851
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2026-03-09 05:32:09 +0100

    Implement removal of empty properties

--- src/main/scala/JsonSchemaPatcher.scala
+++ src/main/scala/JsonSchemaPatcher.scala
@@ -1,5 +1,7 @@
 package patcher
 
+import scala.annotation.tailrec
+
 import io.circe.Json
 import io.circe.JsonObject
 import io.circe.parser.parse
@@ -18,6 +20,19 @@ case class JsonSchemaPatcher(json: JsonObject):
       }
     }
 
+  def dropEmptyOverrides: JsonSchemaPatcher =
+    def isEmpty(o: Json): Boolean =
+      o.asObject.map(_.isEmpty).getOrElse(false)
+    JsonSchemaPatcher(
+      _forEachDefinition(o =>
+        val derivedProperties = _derivedProperties(o)
+        _forEachProperty((key, value) =>
+          if isEmpty(value) && derivedProperties.contains(key) then { None }
+          else { Some(value) },
+        )(o),
+      )(json),
+    )
+
   def _modifyAll(f: Json => Json): JsonSchemaPatcher =
     JsonSchemaPatcher(
       Plated
@@ -26,7 +41,73 @@ case class JsonSchemaPatcher(json: JsonObject):
         .get,
     )
 
+  def _forEachDefinition(f: JsonObject => JsonObject)(
+      j: JsonObject,
+  ): JsonObject =
+    def modifyEachDefinition(o: JsonObject): Json =
+      o.mapValues(o => f(o.asObject.get).toJson).toJson
+    j.toJson.hcursor
+      .downField(definition_path)
+      .withFocus(_.withObject(modifyEachDefinition))
+      .top
+      .flatMap(_.asObject)
+      .get
+
+  def _forEachProperty(f: ((String, Json)) => Option[Json])(
+      j: JsonObject,
+  ): JsonObject =
+    def modifyEachProperty(o: JsonObject): Json =
+      JsonObject
+        .fromIterable(
+          o.toMap.flatMap((k, v) =>
+            f(k -> v) match
+              case Some(res) => Some(k -> res)
+              case None      => None,
+          ),
+        )
+        .toJson
+    j.toJson.hcursor
+      .downField("properties")
+      .withFocus(_.withObject(modifyEachProperty))
+      .top
+      .flatMap(_.asObject)
+      .get
+
+  def _derivedProperties(o: JsonObject): List[String] =
+    def getParents(o: Json): List[String] =
+      root.allOf.each.`$ref`.as[String]
+        .getAll(o)
+        .map(getDefinitionName)
+    def getProperties(definition: String) =
+      _getDefinition(definition).asObject
+        .flatMap(
+          _("properties").flatMap(_.asObject),
+        )
+        .toList
+        .flatMap(_.keys)
+    @tailrec
+    def collectProperties(
+        definitions: List[String],
+        properties: List[String],
+    ): List[String] =
+      definitions match
+        case x :: xs =>
+          collectProperties(
+            getParents(_getDefinition(x)) ::: xs,
+            getProperties(x) ::: properties,
+          )
+        case Nil => properties
+    collectProperties(getParents(o.toJson), List())
+
+  def _getDefinition(definition: String): Json =
+    json.toJson.hcursor
+      .downField(definition_path)
+      .downField(definition)
+      .focus
+      .get
+
 object JsonSchemaPatcher:
+
   def fromString(json: String): Option[JsonSchemaPatcher] =
     parseJson(json).map(JsonSchemaPatcher.apply)
 
@@ -40,8 +121,8 @@ object BadMap:
         (
           "properties",
           JsonObject(
-            ("@type", createRef(s"#/${definition_path}/mobidp.common.String")),
-            ("key", createRef(s"#/${definition_path}/mobidp.common.String")),
+            ("@type", createRef(createDefinition("mobidp.common.String"))),
+            ("key", createRef(createDefinition("mobidp.common.String"))),
             ("value", createRef(ref)),
           ).toJson,
         ),
@@ -71,6 +152,12 @@ case class FixedMap(type_ref: String):
 def createRef(ref: String): Json = JsonObject(
   ("$ref", Json.fromString(ref)),
 ).toJson
+
+def getDefinitionName(s: String): String =
+  s.reverse.takeWhile(_ != '/').reverse
+
+def createDefinition(definition: String): String =
+  s"#/${definition_path}/${definition}"
 
 val definition_path = "definitions"
 
